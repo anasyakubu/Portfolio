687a56cb3dbee6a9c1ea08c9a7bc8b926d353fb7
anasyakubu <yakubuanas04@gmail.com>
2024-06-09 00:02:12 +0100

    Hero Section

--- src/Section/Hero/Hero.jsx
+++ src/Section/Hero/Hero.jsx
@@ -12,16 +12,29 @@ const Hero = () => {
       <div className="p-32 lg:py-10">
         <div className="">
           <h6 className="text-[#aeaeae]">Hi 👋, my name is</h6>
-          <h1 className="mt-2 text-7xl font-extrabold text-[#f8f8f8]">
+          <h1 className="mt-5 text-7xl font-extrabold text-[#f8f8f8]">
             Anas Yakubu.
           </h1>
-          <h1 className="mt-5 text-7xl font-extrabold text-[#aeaeae]">
-            It`s Not Magic, <br /> It`s Programming.
+          <h1
+            className="mt-5 text-7xl font-extrabold text-[#aeaeae]"
+            // style={{ lineHeight: "5rem" }}
+          >
+            It`s Not Magic, It`s Programming.
           </h1>
-          <p className="mt-5 text-[#aeaeae]">
-            I`m a Self-taught Software Enigineer with a passion for thoughtful
-            UI design, collaboration, and building.
+          <p className="mt-10 text-lg font-light text-[#aeaeae]">
+            I am a <span className="text-[#f8f8f8]">software engineer</span>{" "}
+            software engineer who enjoys seamlessly bridging the gap between{" "}
+            <span className="text-[#f8f8f8]">people and digital space</span> by
+            day, and a <span className="text-[#f8f8f8]">anime</span> lover.
           </p>
+          <div className="mt-10 flex gap-5">
+            <button className="p-3 pr-6 pl-6 rounded-full bg-[#f8f8f8] text-black text-sm">
+              Get In Touch
+            </button>
+            <button className="p-3 pr-6 pl-6 rounded-full bg-transparent text-[#f8f8f8] border-[#f8f8f8] border text-sm">
+              View Resume
+            </button>
+          </div>
         </div>
       </div>
     </div>
