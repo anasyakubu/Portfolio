a9ba3aa27f9a8e4eb85ce5ddd29479749f9456b3
anasyakubu <yakubuanas04@gmail.com>
2024-06-09 13:01:47 +0100

    changes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@
 
 import AboutSection from "../../Section/AboutSection/AboutSection";
 import Hero from "../../Section/Hero/Hero";
+import ProjectSection from "../../Section/ProjectSection/ProjectSection";
 
 const Home = () => {
   return (
     <div className="Home">
       <Hero />
       <AboutSection />
+      <ProjectSection />
     </div>
   );
 };
---
+++ src/Section/ProjectSection/ProjectSection.jsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+
+const ProjectSection = () => {
+  return (
+    <div className="ProjectSection bg-[#1b1b1b] text-[#aeaeae]">
+      <div className="p-24">
+        <div className="">
+          <h6 className="text-[#aeaeae] text-3xl">Projects</h6>
+          <p className="mt-3 text-md">
+            This space contains some of the things i`ve built to solve some of
+            the problems i find frustrating or others find as well. Other random
+            projects can be find in my Universe.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSection;
