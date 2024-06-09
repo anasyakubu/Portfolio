190e9dc36b46bd978a2f7b4ddacc2d9f3c8cc343
anasyakubu <yakubuanas04@gmail.com>
2024-06-09 16:17:05 +0100

    Project section

---
+++ src/Components/ProjectCards.jsx
@@ -0,0 +1,46 @@
+// import React from 'react'
+import { FiExternalLink } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
+const ProjectCards = ({ Image, title, details, link, live, color }) => {
+  return (
+    <div className="ProjectCards">
+      <div className="p-2 m-2">
+        <div className="">
+          <img className="rounded-md" src={Image} alt="Project Image" />
+          <div className="">
+            <h6 className="mt-3">{title}</h6>
+            <p className="mt-3 text-sm">{details}</p>
+            <div className="mt-3 flex justify-between">
+              <div
+                className={`text-[10px] min-w-[50px] px-[8px] py-1 rounded-[30px] ${color} text-white ppR flex items-center justify-between gap-2`}
+              >
+                {live}
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  strokeWidth="0"
+                  viewBox="0 0 16 16"
+                  className="text-green-400"
+                  height="8"
+                  width="8"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle cx="8" cy="8" r="8"></circle>
+                </svg>
+              </div>
+              <Link to={link} target="_blank" className="text-sm flex gap-2">
+                <span>Vist</span>
+                <span>
+                  <FiExternalLink />
+                </span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCards;
--- src/Section/ProjectSection/ProjectSection.jsx
+++ src/Section/ProjectSection/ProjectSection.jsx
@@ -1,4 +1,17 @@
 // import React from 'react'
+import "./ProjectSection.scss";
+import invoice from "../../assets/invoice.png";
+import ai from "../../assets/ai-sass.png";
+import prompt from "../../assets/ai-prompt.png";
+import task from "../../assets/task-manager.png";
+import healty from "../../assets/healthy.png";
+import foc from "../../assets/focai.png";
+import portifolo from "../../assets/portifiolo-2.png";
+// import dbrs from "../../assets/dbrs.png";
+import blog from "../../assets/blog.png";
+
+import ProjectCards from "../../Components/ProjectCards";
+import { Link } from "react-router-dom";
 
 const ProjectSection = () => {
   return (
@@ -11,6 +24,80 @@ const ProjectSection = () => {
             the problems i find frustrating or others find as well. Other random
             projects can be find in my Universe.
           </p>
+        </div>
+        <div className="mt-10">
+          <div className="p-1 space-y-2 lg:grid lg:grid-cols-4 lg:gap-x-6 lg:space-y-0">
+            <ProjectCards
+              Image={invoice}
+              title="Daily Invoice"
+              details="Professional and Easy-to-use Online Invoicing Software for Startups, Business Owners and Freelancers."
+              link="https://daily-invoice.vercel.app/"
+              live="Live"
+              color="bg-green-600"
+            />
+            <ProjectCards
+              Image={ai}
+              title="Titan AI"
+              details=" AI Tools to Share Your Knowledge and Empower Youself"
+              link="https://titan-orpin.vercel.app/"
+              live="Live"
+              color="bg-green-600"
+            />
+            <ProjectCards
+              Image={prompt}
+              title="Promptee"
+              details="An Open-source AI Prompting tool for modern world to discover, create and sharing creative prompts"
+              link="https://ai-prompts-app-nine.vercel.app/"
+              live="Live"
+              color="bg-green-600"
+            />
+            <ProjectCards
+              Image={blog}
+              title="Blog 3"
+              details="A blog platform to share and write article all around the world"
+              link="https://blog-3-post.vercel.app/"
+              live="Live"
+              color="bg-green-600"
+            />
+            <ProjectCards
+              Image={healty}
+              title="Healthy"
+              details="A website for healthy food and diets"
+              link="https://healthy-website-psi.vercel.app/"
+              live="Live"
+              color="bg-green-600"
+            />
+            <ProjectCards
+              Image={task}
+              title="Devograph"
+              details="Devograph, your ultimate task manager software designed to streamline your workflow and enhance your productivity."
+              link="https://devograph.vercel.app/login"
+              live="Live"
+              color="bg-green-600"
+            />
+            <ProjectCards
+              Image={foc}
+              title="FocGPT"
+              details="An AI App for studying for Faculty of Computing BUK"
+              link="https://foc-ai.vercel.app/"
+              live="Live"
+              color="bg-green-600"
+            />
+            <ProjectCards
+              Image={portifolo}
+              title="First Portifolio"
+              details="My first portifolio design using Reactjs"
+              link=""
+              live="Not-Live"
+              color="bg-red-500"
+            />
+          </div>
+        </div>
+        <div className="mt-5">
+          <button className="p-3 pr-6 pl-6 rounded-full bg-[#f8f8f8] text-black text-sm hover:bg-transparent border border-[#f8f8f8] hover:text-[#f8f8f8] hover:border-[#f8f8f8]">
+            <Link to="/Project"></Link>
+            View More
+          </button>
         </div>
       </div>
     </div>
