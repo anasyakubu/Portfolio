b85ea01ca3b79ccc7031896e24a223860b848cf9
anasyakubu <yakubuanas04@gmail.com>
2024-07-13 20:24:46 +0100

    Subscribers API changes

--- src/Section/Subscribers/Subscribers.jsx
+++ src/Section/Subscribers/Subscribers.jsx
@@ -23,7 +23,7 @@ const Subscribers = () => {
     try {
       setLoading(true);
       const { data } = await axios.post(
-        "https://api-anasyakubu-cms.vercel.app/subscribers/add",
+        "https://api-anasyakubu-cms.vercel.app/subscriber/add",
         //"https://anasyakubu-cms-api.onrender.com/createSubscribers",
         {
           email,
